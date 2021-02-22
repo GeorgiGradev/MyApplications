2eb8b8e6727a7345312c6bfd17e50f3d3936306c
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-02-22 22:49:40 +0200

    Add new projects

---
+++ P01_Snake_Game/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char wallSymbol = '\u25A0';
+
+        public Wall(int leftX, int topY)
+            : base(leftX, topY)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            InitializeWallBorders();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(62, 1);
+            Console.WriteLine("Result: 0");
+
+        }
+
+        private void SetHorizontalLine(int topY)
+        {
+            for (int leftX = 0; leftX < this.LeftX; leftX++)
+            {
+                this.Draw(leftX, topY, wallSymbol);
+            }
+        }
+
+        private void SetVerticalLine(int leftX)
+        {
+            for (int topY = 0; topY < this.TopY; topY++)
+            {
+                this.Draw(leftX, topY, wallSymbol);
+            }
+        }
+
+        private void InitializeWallBorders()
+        {
+            SetHorizontalLine(0);
+            SetHorizontalLine(this.TopY);
+
+            SetVerticalLine(0);
+            SetVerticalLine(this.LeftX - 1);
+        }
+
+        public bool IsPointOfWall(Point snake)
+        {
+            return snake.TopY == 0 || snake.LeftX == 0 || snake.TopY == this.TopY || snake.LeftX == this.LeftX - 1;
+        }
+    }
+}
